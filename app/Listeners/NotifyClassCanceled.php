fc2c7bfaa8ef231551d086fed9d2b90a330a7740
KSHDestiny <kaungsathein.5899@gmail.com>
2024-04-18 02:20:02 +0630

    using notification

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -4,9 +4,11 @@ namespace App\Listeners;
 
 use App\Events\ClassCanceled;
 use App\Mail\ClassCanceledMail;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ClassCanceledNotification;
 
 class NotifyClassCanceled
 {
@@ -23,14 +25,18 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $members = $event->scheduledClass->members();
+        // $members = $event->scheduledClass->members();
+        // $className = $event->scheduledClass->classType->name;
+        // $classDateTime = $event->scheduledClass->date_time;
+        // $details = compact('className','classDateTime');
+        // $members->each(function($user) use ($details){
+        //     Mail::to($user)->send(new ClassCanceledMail($details));
+        // });
 
+        $members = $event->scheduledClass->members()->get();
         $className = $event->scheduledClass->classType->name;
         $classDateTime = $event->scheduledClass->date_time;
         $details = compact('className','classDateTime');
-
-        $members->each(function($user) use ($details){
-            Mail::to($user)->send(new ClassCanceledMail($details));
-        });
+        Notification::send($members, new ClassCanceledNotification($details));
     }
 }
